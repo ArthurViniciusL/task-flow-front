7017fdda50d2afe179ffa66e0bd1c07aca3f0404
arthurviniciussl <arthurviniciussl.contato@gmail.com>
2025-11-07 08:41:47 -0300

    fixed dependencys

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 adiciona aqui
+  ],
   server: {
     port: 5173,
   },
